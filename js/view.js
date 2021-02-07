6ccd22ad11eef25f89d9a0fcf1920e67c34d9b7d
Rose Gulab <rosegulab505@gmail.com>
2021-02-07 10:57:10 +0500

    changes in view chapter,map file

--- js/view.js
+++ js/view.js
@@ -1,49 +1,75 @@
 
-let slidePosition = 0;
-const slides = document.getElementsByClassName("col-1");
-const totalSlide = slides.length;
+// let slidePosition = 0;
+// const slides = document.getElementsByClassName("col-1");
+// const totalSlide = slides.length;
 
-console.log(slides);
+// console.log(slides);
 
-document.getElementById("prev");
+// document.getElementById("prev");
 
-function prev(){
-    moveTonextSlide();
-}    
+// function prev(){
+//     moveTonextSlide();
+// }    
 
 
-document.getElementById("next");
+// document.getElementById("next");
 
-function next(){
-    moveToprevSlide();
-}
+// function next(){
+//     moveToprevSlide();
+// }
 
-function updateSlidePosition(){
+// function updateSlidePosition(){
+//     for(let slide of slides){
+//         slide.classList.remove("carosel-image-visible");
+//         slide.classList.add("carosel-image-hidden");
+//     }
+
+//     slides[slidePosition].classList.add("carosel-image-visible");
+// }
+
+// function moveTonextSlide(){
+//     if(slidePosition === totalSlide - 1){
+//         slidePosition = 0;
+//     }
+//     else{
+//         slidePosition++;
+//     }
+//     updateSlidePosition();
+// }
+
+
+// function moveToprevSlide(){
+//     if(slidePosition === 0){
+//         slidePosition = totalSlide - 1;
+//     }
+//     else{
+//         slidePosition--;
+//     }
+//     updateSlidePosition();
+// }
+
+
+let prev = document.getElementById("prev");
+let next = document.getElementById("next");
+let prevleft = document.getElementsByClassName("button-left");
+
+function nextBtn(){
+    let slides = document.getElementsByClassName("col-1");
+    // let wrapper = document.getElementsByClassName("images-container");
+    let totalSlide = slides.length;
+    console.log(slides);
     for(let slide of slides){
-        slide.classList.remove("carosel-image-visible");
-        slide.classList.add("carosel-image-hidden");
-    }
+        console.log("before if");
+        // slide.style.display = "inline-block";
 
-    slides[slidePosition].classList.add("carosel-image-visible");
-}
-
-function moveTonextSlide(){
-    if(slidePosition === totalSlide - 1){
-        slidePosition = 0;
+        // slide.style.width = "5000px";
+        // slide.style.overflow = "scroll";
+        if(totalSlide - 1){
+            console.log("after if");
+            slide.style.display = "block";
+            // slide.style.overflowX = "scroll";
+            // slide.style.overflowY = "hidden";    
+        }
+        
     }
-    else{
-        slidePosition++;
-    }
-    updateSlidePosition();
-}
-
-
-function moveToprevSlide(){
-    if(slidePosition === 0){
-        slidePosition = totalSlide - 1;
-    }
-    else{
-        slidePosition--;
-    }
-    updateSlidePosition();
 }
